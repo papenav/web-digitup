c79d937803c2e6d3b24c5711d6fb2df66906efbb
Pedro Peña <ext_papenav@falabella.cl>
2026-03-27 19:34:28 -0300

    feat: add sitemap

---
+++ app/sitemaps.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://digitup.cl",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://digitup.cl/contacto",
+      lastModified: new Date(),
+    },
+  ];
+}
